11399e2d407dc190a00f0fde989197846806029b
Tejas12 <tejasmalviya1511@gmail.com>
2022-04-13 09:05:07 +0530

    New Pyton file

---
+++ python/maths.py
@@ -0,0 +1,10 @@
+class mobile:
+    def dial_number(self):
+        print("dialing number")
+    
+    def play_game(self):
+        print("playing games")
+        
+m1=mobile
+m1.self.dial_number()
+
