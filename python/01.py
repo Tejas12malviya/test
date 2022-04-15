a975f36888b8d6b7425ff676c0cb57347528a7b6
Tejas12 <tejasmalviya1511@gmail.com>
2022-04-15 08:26:43 +0530

    some change

--- python/01.py
+++ python/01.py
@@ -1,29 +1,29 @@
-# a=input['Enter the value of a:']
-# b=input['Enter the value of b:']
-# c=input['Enter the value of c:']
-# if(a>b)(
-#     if(a>c){
-#         print("a is greatest")
-#     }
-#     else{
-#         print("c is greatest")
-#     })
-# else [
-#     print("b is greatest")
-# ]
-# a=input['Enter the value of a:']
-# b=input['Enter the value of b:']
-# c=input['Enter the value of c:']
-# if(a>b):
-#     if(a>c):
-#         print("a is greatest")
+a=input['Enter the value of a:']
+b=input['Enter the value of b:']
+c=input['Enter the value of c:']
+if(a>b)(
+    if(a>c){
+        print("a is greatest")
+    }
+    else{
+        print("c is greatest")
+    })
+else [
+    print("b is greatest")
+]
+a=input['Enter the value of a:']
+b=input['Enter the value of b:']
+c=input['Enter the value of c:']
+if(a>b):
+    if(a>c):
+        print("a is greatest")
     
-#     else:
-#         print("c is greatest")
+    else:
+        print("c is greatest")
     
 
-# else:
-#     print("b is greatest")
+else:
+    print("b is greatest")
 a=input['Enter the value of a:']
 b=input['Enter the value of b:']
 c=input['Enter the value of c:']
